70ccdfccfed0df2122f3ca6255fc8f82246f3d5c
vagrant <vagrant>
2015-04-24 20:40:32 -0500

    Karma coverage is operational.

---
+++ tests/test-main.js
@@ -0,0 +1,36 @@
+var tests = [];
+for (var file in window.__karma__.files) {
+    // Our test modules are named "<something>Spec.js"
+    // If you decide to change the format of the file name this Regex
+    // must reflect it.
+    if (/Spec\.js$/.test(file)) {
+        tests.push(file);
+    }
+}
+
+requirejs.config({
+    // Karma serves files from '/base'
+    // The "src/" part is the same as the one in the repo root
+    baseUrl: '/base/app',
+
+    // Usual RequireJS stuff...
+    paths: {
+        jquery: '../assets/library/components/jquery/dist/jquery.min',
+        underscore: '../assets/library/components/underscore/underscore',
+        backbone: '../assets/library/components/backbone/backbone',
+        marionette: '../assets/library/components/marionette/lib/backbone.marionette.min',
+        handlebars: '../assets/library/components/handlebars/handlebars.min',
+        text: '../assets/library/require.text'
+    },
+    shim: {
+        'underscore': {
+            exports: '_'
+        }
+    },
+
+    // ask Require.js to load these files (all our tests) that we collected before
+    deps: tests,
+
+    // start test run, once Require.js is done
+    callback: window.__karma__.start
+});
