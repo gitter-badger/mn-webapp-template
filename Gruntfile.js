8dd2cb84001f9e222b2bfd40cfdac3bdeb898ccd
Jason Wohlgemuth <jason.h.wohlgemuth@gmail.com>
2015-05-29 20:04:59 -0500

    Troubleshooting Travis CI build...

--- Gruntfile.js
+++ Gruntfile.js
@@ -128,6 +128,7 @@ module.exports = function(grunt) {
                 src: ['app/**/*.js'],
                 options: {
                     specs: ['tests/jasmine/specs/**/*.js'],
+                    keepRunner: true,
                     template: require('grunt-template-jasmine-requirejs'),
                     templateOptions: {
                         requireConfigFile: 'app/config.js',
