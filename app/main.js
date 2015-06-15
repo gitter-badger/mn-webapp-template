b10ca2f1c982af5f6754ccdf803d55aff953631d
Jason Wohlgemuth <jason.h.wohlgemuth@gmail.com>
2015-06-15 14:47:59 -0500

    Testing text plugin with HTML and JSON files...part 2 (now with parsing)

--- app/main.js
+++ app/main.js
@@ -11,6 +11,8 @@ define(function(require) {
 
     var tpl = require('text!../../assets/templates/table.html');
     var jso = require('text!../../tests/data/json/users.json');
+    var data = JSON.parse(jso);
+    console.log(data[3]);
 
     WebApp.on('start', function() {
         console.info('Please read the source and/or generated documentation for more information.');
